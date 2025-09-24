58c6979986edeaffac5e39d6ee651e226aa1b0c9
pkv-woodstock <pkvstarscream@gmail.com>
2025-09-24 10:12:34 +0530

    signup controller added

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,64 @@
+import { generateToken } from '../lib/utils.js';
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body; // get data from request body
+
+    try {
+        if((!fullName) || (!email) || (!password)){
+            return res.status(400).json({ message: "All fields are required!" });
+        }
+        // check password length
+        if(password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long!" });
+        }
+        // check if email is valid using regex
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({ message: "Please enter a valid email address!" });
+        }
+        // check if user already exists
+        const user = await User.findOne({ email });
+        if(user){
+            return res.status(400).json({ message: "Email already exists! Login instead or use different mail for signup" });
+        }
+
+        // create new user
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName, 
+            email,
+            password: hashedPassword
+        })
+
+        if (newUser) {
+            // generate JWT token for user
+            generateToken(newUser._id, res)
+            await newUser.save();
+            return res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            })
+        }
+        else {
+            res.status(400).json({ message: "Invalid user details!" });
+        }
+    }
+    catch (error) {
+        console.error("Error in signup: ", error);
+        res.status(500).json({ message: "Server error!" });
+    }
+}
+
+export const login = (req, res) => {
+    res.send('Login endpoint');
+}
+
+export const logout = (req, res) => {
+    res.send('Logout endpoint');
+}
