8a90f71a47a35b835b4856f3d5e1fa481faf1db8
pkv-woodstock <pkvstarscream@gmail.com>
2025-09-24 08:20:04 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 // basic API
 import express from 'express';
 import dotenv from 'dotenv'
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -8,10 +9,19 @@ import messageRoutes from './routes/message.route.js';
 dotenv.config()
 
 const app = express();
+const __dirname = path.resolve()
 const PORT = process.env.PORT || 3000;
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+// make it ready for deplyment
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
